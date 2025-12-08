9a964b25972b56edce634275e13eaf3a7e6535c4
Antonio <antoniocutri841@gmail.com>
2025-12-08 15:17:55 +0100

    create navbare component and installed react router

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Main from './components/Main'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
+      <Navbar/>
       <Main>
         <Main.Counter/>
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <div className="navBar">
+            <ul className="nav_list">
+                <li className="nav_item">
+                    <a href="" className="nav_link">Homepage</a>
+                </li>
+                <li className="nav_item">
+                    <a href="" className="nav_link">Post</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
