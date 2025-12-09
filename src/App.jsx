f03a97bb1c562474ac4a1337c3159053808342de
Antonio <antoniocutri841@gmail.com>
2025-12-09 21:40:44 +0100

    corretti errori

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Main from './components/Main'
 import { BrowserRouter, Routes, Route } from "react-router";
 import Homepage from './views/Homepage'
 import Layout from './components/Layout'
 import DetailView from './views/DetailView'
+import Counter from './views/Counter'
+import Form from './views/Form'
+import LoadingData from './views/LoadingData'
 
 function App() {
 
@@ -17,9 +19,9 @@ function App() {
           <Route path={"/"} element={<Layout />} >
             
             <Route index element={<Homepage />} />
-            <Route path={"/counter"} element={<Main.Counter/>} />
-            <Route path={"/form"} element={<Main.Form />} />
-            <Route path={"/posts"} element={<Main.LoadingData/>} />
+            <Route path={"/counter"} element={<Counter/>} />
+            <Route path={"/form"} element={<Form />} />
+            <Route path={"/posts"} element={<LoadingData/>} />
             <Route path={"/post/:id"} element={<DetailView/>} />
 
           </Route>
